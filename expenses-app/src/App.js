e1dc57bca14426754b366093134442c3146bb763
jcarlo130 <jan.pastor@ciit.edu.ph>
2024-07-15 23:24:24 +0800

    Color and BG

--- expenses-app/src/App.js
+++ expenses-app/src/App.js
@@ -1,9 +1,11 @@
-import './app.css';
+import "./app.css";
+import logo from "./Assets/logo.svg";
+
+// Use camel case for naming classes. e.g.: btnLoginContainer
 
 function App() {
   return (
     <div className="app">
-
     </div>
   );
 }
